ade734dc01267f4eaed372057d9175b27cb65c6b
Jose Carlos Sancho <jcsanchop@gmail.com>
2020-10-05 19:40:41 +0200

    changes

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const BN = require('bn.js');
 
 const truffleAssert = require('truffle-assertions');
 
-console.log('starting')
+console.log('starting...')
 
 contract("Testing ", async(accounts) => {
 
@@ -15,8 +15,8 @@ contract("Testing ", async(accounts) => {
    contractTrojan = await TrojanCoinAfter.deployed();
    console.log('Contract address: ', contractTrojan.address)
    let owner = await contractTrojan.owner.call()
-   console.log("Owner: ", owner,account0, account1);
-   assert.equal(owner, account0, "accounts does not match")
+   console.log("Owner: ", owner, account1);
+   assert.equal(owner, account0, "owner accounts does not match")
  })
 
  it("#2 check transfer", async function() {
@@ -30,12 +30,13 @@ contract("Testing ", async(accounts) => {
         assert.notEqual(res.toString(), 0, "Transfer was not successful")
  })
 
-
+/*
  it("#3 check price", async function() {
          let answer = 0;
          answer=parseInt(await contractTrojan.getAnswers.call());
          console.log("rate: " + answer)
          assert.notEqual(answer.toString(), 0, "Price was not successful")
  })
+*/
 
 });
